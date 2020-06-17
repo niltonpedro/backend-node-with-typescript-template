c9683cd9fcd8c9648f61292fb116f698fc690fc3
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2020-06-16 21:34:05 -0300

    Node ts added

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();  
+
+app.get('/', (resquest, response) => {
+  return response.json({
+    message: 'rodou mesmo'
+  })
+})
+
+app.listen(3333, () => {
+  console.log('Servidor deu bom')
+})
