4922e8e84959a3eb4dc9f7515f3d9f1c5c0cf05a
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2020-06-16 22:12:00 -0300

    Prettier configs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (resquest, response) => response.json({
-  message: 'rodou mesmo',
-}));
+routes.get('/', (resquest, response) =>
+  response.json({
+    message: 'rodou mesmo',
+  }),
+);
 
 export default routes;
